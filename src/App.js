32f195dd81cabf4cbb12573f75514596334c6dff
Tara Sherman <taradelin@gmail.com>
2019-12-26 19:12:56 -0500

    Pulling in props, instead of hard-coding state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
-import UserItem from './components/users/UserItem';
+import Users from './components/users/Users';
 import './App.css';
 
 export class App extends Component {
@@ -9,7 +9,7 @@ export class App extends Component {
     return (
       <div className="App">
         <Navbar />
-        <UserItem />
+        <Users />
       </div>
     );
   }
