9291404c0758570522b3fc5000de7d4221a8fd8b
Tara Sherman <taradelin@gmail.com>
2019-12-30 16:57:42 -0500

    fetching data with axios/promises

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import axios from 'axios';
 import './App.css';
 
 export class App extends Component {
-  render() {
+componentDidMount() {
+  axios.get('https://api.github.com/users').then(res => console.log(res.data));
+}
 
+  render() {
     return (
       <div className="App">
         <Navbar />
