d5b9f5fbc4e54383209e3ae7f6c0ba5462e0b325
Tara Sherman <taradelin@gmail.com>
2020-01-01 10:01:42 -0500

    showClear button and functionality

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 export class Search extends Component {
   state = {
     text: ''
+  }
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired
   }
 
   onSubmit = (event) => {
@@ -32,6 +38,11 @@ export class Search extends Component {
             value='Search'
             className='btn btn-dark btn-block' />
         </form>
+        {this.props.showClear && (
+          <button className="btn btn-light btn-block" onClick={this.props.clearUsers}>
+            Clear
+          </button>
+        )}
       </div>
     )
   }
