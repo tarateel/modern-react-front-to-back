55c560f169ea72fe4cb00102e18686b8b0431446
Tara Sherman <taradelin@gmail.com>
2020-01-01 12:05:00 -0500

    Added Alert if search is missing text

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
+import Alert from './components/layout/Alert.js';
 import axios from 'axios';
 import './App.css';
 
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   }
 
   // async componentDidMount() {
@@ -29,16 +31,23 @@ class App extends Component {
 
   clearUsers = () => this.setState({ users: [], loading: false});
 
+  setAlert = (msg, type) => {
+    this.setState({ alert: {msg, type }});
+  }
+
   render() {
     const { users, loading } = this.state;
     return (
       <div className="App">
         <Navbar />
         <div className="container">
+          <Alert
+            alert={this.state.alert} />
           <Search
             searchUsers={this.searchUsers}
             clearUsers={this.clearUsers}
-            showClear={users.length > 0 ? true : false} />
+            showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert} />
           <Users
             loading={loading}
             users={users} />
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -8,13 +8,18 @@ export class Search extends Component {
   static propTypes = {
     searchUsers: PropTypes.func.isRequired,
     clearUsers: PropTypes.func.isRequired,
-    showClear: PropTypes.bool.isRequired
-  }
+    showClear: PropTypes.bool.isRequired,
+    setAlert: PropTypes.func.isRequired
+  };
 
   onSubmit = (event) => {
     event.preventDefault();
-    this.props.searchUsers(this.state.text);
-    this.setState({ text: '' });
+    if(this.state.text === '') {
+      this.props.setAlert('Please enter something', 'Light');
+    } else {
+      this.props.searchUsers(this.state.text);
+      this.setState({ text: '' });
+    }
   }
 
   onChange = (event) => {
