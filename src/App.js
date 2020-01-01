16799c21d789e132e6e0ea283206ede415be7d50
Tara Sherman <taradelin@gmail.com>
2020-01-01 12:06:49 -0500

    Added 5 second timeout to Alert

--- src/App.js
+++ src/App.js
@@ -33,6 +33,7 @@ class App extends Component {
 
   setAlert = (msg, type) => {
     this.setState({ alert: {msg, type }});
+    setTimeout(() => this.setState({ alert: null}), 5000)
   }
 
   render() {
